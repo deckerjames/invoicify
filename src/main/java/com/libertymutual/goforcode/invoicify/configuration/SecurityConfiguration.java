1fc20e482e09434342c74dbb550763191edf0efe
The Overlord <james.decker@libertymutual.com>
2017-09-13 14:51:05 -0700

    added lots of crap

--- src/main/java/com/libertymutual/goforcode/invoicify/configuration/SecurityConfiguration.java
+++ src/main/java/com/libertymutual/goforcode/invoicify/configuration/SecurityConfiguration.java
@@ -1,43 +1,45 @@
 package com.libertymutual.goforcode.invoicify.configuration;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.libertymutual.goforcode.invoicify.services.InvoicifyUserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private InvoicifyUserDetailsService userDetailsService;
+    
+    public SecurityConfiguration(InvoicifyUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests()
-        .antMatchers("/", "/css/**", "/js/**").permitAll()
-        .antMatchers("/invoices/**").hasAnyRole("ADMIN", "ACCOUNTANT").anyRequest().authenticated()
-        .antMatchers("/billing-records/**").hasAnyRole("ADMIN", "CLERK").anyRequest().authenticated()
-        .antMatchers("/admin/**").hasRole("ADMIN").anyRequest().authenticated().and()
+        .antMatchers("/", "/css/**", "/js/**", "/signup").permitAll()
+        .antMatchers("/invoices/**").hasAnyRole("ADMIN", "ACCOUNTANT")
+        .antMatchers("/billing-records/**").hasAnyRole("ADMIN", "CLERK")
+        .antMatchers("/admin/**").hasRole("ADMIN")
+        .anyRequest().authenticated().and()
         .formLogin();
     }
-
+    
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        
-        UserDetails 
-        user = User.withUsername("admin").password("admin").roles("ADMIN").build();
-        manager.createUser(user);
-        
-        user = User.withUsername("clerk").password("clerk").roles("CLERK").build();
-        manager.createUser(user);
-        
-        user = User.withUsername("accountant").password("accountant").roles("ACCOUNTANT").build();
-        manager.createUser(user);
-        
-        return manager;
+        return userDetailsService;
     }
 
 }
--- src/main/java/com/libertymutual/goforcode/invoicify/controllers/HomeController.java
+++ src/main/java/com/libertymutual/goforcode/invoicify/controllers/HomeController.java
@@ -1,16 +1,56 @@
 package com.libertymutual.goforcode.invoicify.controllers;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.libertymutual.goforcode.invoicify.models.User;
+import com.libertymutual.goforcode.invoicify.repositories.UserRepository;
 
 @RequestMapping("/")
 @Controller
 public class HomeController {
     
+    private UserRepository userRepository;
+    private PasswordEncoder encoder;
+    
+    public HomeController(UserRepository userRepository, PasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+    
     @GetMapping("")
     public String homePage() {
         return "home/default";
     }
+    
+    @GetMapping("signup")
+    public String signup() {        
+        return "home/signup";
+    }
 
+    @PostMapping("signup")
+    public ModelAndView handleSignup(User user) {  
+        // TODO: THIS IS REALLY STUPID; NEEDS REFACTORING
+        String password = user.getPassword();
+        String encryptedPassword = encoder.encode(password);
+        user.setPassword(encryptedPassword);
+        
+        ModelAndView mv = new ModelAndView();
+        try {
+            userRepository.save(user);
+            mv.setViewName("redirect:/login");
+        } catch (DataIntegrityViolationException dive) {
+            mv.setViewName("home/signup");
+            mv.addObject("errorMessage", "Cannot sign up with that username");
+            
+            
+        }
+        return mv;
+    }
 }
